53014444436e466fc5ce57c8a070e8fb24a2636d
gangelo <web.gma@gmail.com>
2023-04-29 06:27:18 -0400

    Remove erroneous comments

--- lib/dsu.rb
+++ lib/dsu.rb
@@ -5,20 +5,6 @@ Dir.glob("#{__dir__}/dsu/support/**/*.rb").each do |file|
   require file
 end
 
-# require_relative 'dsu/support/interactive/cli'
-# require_relative 'dsu/support/ask'
-# require_relative 'dsu/support/colors'
-# require_relative 'dsu/support/commander/command'
-# require_relative 'dsu/support/configuration'
-# require_relative 'dsu/support/entries_loader'
-# require_relative 'dsu/support/entries_version'
-# require_relative 'dsu/support/entries'
-# require_relative 'dsu/support/entry'
-# require_relative 'dsu/support/field_errors'
-# require_relative 'dsu/support/folder_locations'
-# require_relative 'dsu/support/say'
-# require_relative 'dsu/support/validate_time'
-# require_relative 'dsu/support/validate_version'
 require_relative 'dsu/cli'
 require_relative 'dsu/version'
 
