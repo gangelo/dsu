0694e2f2e5c88804cf88dd3095153508f473a3f1
gangelo <web.gma@gmail.com>
2023-05-07 07:40:58 -0400

    Refactor top-level command options

--- lib/dsu/cli.rb
+++ lib/dsu/cli.rb
@@ -7,6 +7,7 @@ require_relative 'models/entry_group'
 require_relative 'services/configuration_loader_service'
 require_relative 'services/entry_group_hydrator_service'
 require_relative 'services/entry_group_reader_service'
+require_relative 'support/colorable'
 require_relative 'subcommands/config'
 require_relative 'version'
 require_relative 'views/entry_group/show'
@@ -16,17 +17,29 @@ module Dsu
   # The `dsu` command.
   #
   class CLI < ::Thor
+    include Support::Colorable
+
     class_option :debug, type: :boolean, default: false
 
-    map %w[--version -v] => :version
-    # map %w[--interactive -i] => :interactive
-    # map %w[--today -t] => :today
+    map %w[add -a] => :add
+    map %w[config -c] => :config
+    map %w[date -d] => :date
+    map %w[today -n] => :today
+    map %w[tomorrow -t] => :tomorrow
+    map %w[version -v] => :version
+    map %w[yesterday -y] => :yesterday
 
     default_command :help
 
     class << self
       def exit_on_failure?
         false
+      end
+
+      def date_option_description
+        <<-DATE_OPTION_DESC
+          Where DATE may be any date string that can be parsed using `Time.parse`. Consequently, you may use also use '/' as date separators, as well as omit thee year if the date you want to display is the current year (e.g. <month>/<day>, or 1/31). For example: `require 'time'; Time.parse('2023-01-02'); Time.parse('1/2') # etc.`
+        DATE_OPTION_DESC
       end
     end
 
@@ -36,27 +49,44 @@ module Dsu
       @configuration = Services::ConfigurationLoaderService.new.call
     end
 
-    desc 'add [OPTIONS] DESCRIPTION',
-      'Adds a dsu entry for the date associated with the given option.'
+    desc 'add, -a [OPTIONS] DESCRIPTION',
+      'Will add a DSU entry having DESCRIPTION to the date associated with the given OPTION.'
     long_desc <<-LONG_DESC
-      TBD
+      NAME
+      \x5
+      `DSU add, -a [OPTIONS] DESCRIPTION` -- will add a DSU entry having DESCRIPTION to the date associated with the given OPTION.
+
+      SYNOPSIS
+      \x5
+      `dsu add, -a [-d DATE|-n|-t|-y] DESCRIPTION`
+
+      OPTIONS:
+      \x5
+      -d DATE: Adds a DSU entry having DESCRIPTION to the DATE.
+
+      \x5 #{date_option_description}
+
+      \x5 -n: Adds a DSU entry having DESCRIPTION to today's date (`Time.now`).
+
+      \x5 -t: Adds a DSU entry having DESCRIPTION to tomorrow's date (`Time.new.tomorrow`).
+
+      \x5 -y: Adds a DSU entry having DESCRIPTION to yesterday's date (`Time.new.yesterday`).
+
     LONG_DESC
     option :date, type: :string, aliases: '-d'
-    option :next_day, type: :boolean, aliases: '-n'
-    option :previous_day, type: :boolean, aliases: '-p'
-    option :today, type: :boolean, aliases: '-t', default: true
+    option :tomorrow, type: :boolean, aliases: '-t'
+    option :yesterday, type: :boolean, aliases: '-y'
+    option :today, type: :boolean, aliases: '-n', default: true
 
     def add(description)
       time = if options[:date].present?
         Time.parse(options[:date])
-      elsif options[:next_day].present?
+      elsif options[:tomorrow].present?
         1.day.from_now
-      elsif options[:previous_day].present?
+      elsif options[:yesterday].present?
         1.day.ago
       elsif options[:today].present?
         Time.now
-      else
-        raise 'No date option specified.'
       end
       entry = Models::Entry.new(description: description)
       CommandServices::AddEntryService.new(entry: entry, time: time).call
@@ -66,10 +96,10 @@ module Dsu
       end
     end
 
-    desc 'today',
-      'Displays the dsu entries for today.'
+    desc 'today, -n',
+      'Displays the DSU entries for today.'
     long_desc <<-LONG_DESC
-      Displays the dsu entries for today. This command has no options.
+      Displays the DSU entries for today. This command has no options.
     LONG_DESC
     def today
       time = Time.now
@@ -79,10 +109,10 @@ module Dsu
       end
     end
 
-    desc 'tomorrow',
-      'Displays the dsu entries for tomorrow.'
+    desc 'tomorrow, -t',
+      'Displays the DSU entries for tomorrow.'
     long_desc <<-LONG_DESC
-      Displays the dsu entries for tomorrow. This command has no options.
+      Displays the DSU entries for tomorrow. This command has no options.
     LONG_DESC
     def tomorrow
       time = Time.now
@@ -92,10 +122,10 @@ module Dsu
       end
     end
 
-    desc 'yesterday',
-      'Displays the dsu entries for yesterday.'
+    desc 'yesterday, -y',
+      'Displays the DSU entries for yesterday.'
     long_desc <<-LONG_DESC
-      Displays the dsu entries for yesterday. This command has no options.
+      Displays the DSU entries for yesterday. This command has no options.
     LONG_DESC
     def yesterday
       time = Time.now
@@ -105,10 +135,10 @@ module Dsu
       end
     end
 
-    desc 'date',
-      'Displays the dsu entries for DATE.'
+    desc 'date, -d DATE',
+      'Displays the DSU entries for DATE.'
     long_desc <<-LONG_DESC
-      Displays the dsu entries for DATE.
+      Displays the DSU entries for DATE.
 
       For example: `require 'time'; Time.parse('2023-01-02'); Time.parse('1/2') # etc.`
 
@@ -122,10 +152,13 @@ module Dsu
         display_entry_group(time: time)
         puts
       end
+    rescue ArgumentError => e
+      say "Error: #{e.message}", ERROR
+      exit 1
     end
 
     # TODO: Implement this.
-    # desc 'interactive', 'Opens a dsu interactive session'
+    # desc 'interactive', 'Opens a DSU interactive session'
     # long_desc ''
     # option :next_day, type: :boolean, aliases: '-n'
     # option :previous_day, type: :boolean, aliases: '-p'
@@ -143,10 +176,12 @@ module Dsu
     #   say 'Done.'
     # end
 
-    desc 'config SUBCOMMAND', 'Manage configuration file for this gem'
+    desc 'config, -c SUBCOMMAND',
+      'Manage configuration file for this gem'
     subcommand :config, Subcommands::Config
 
-    desc '--version, -v', 'Displays this gem version'
+    desc 'version, -v',
+      'Displays this gem version'
     def version
       say VERSION
     end
@@ -159,9 +194,9 @@ module Dsu
       say 'Interactive Mode Commands:'
       say '---'
       say '[h]: show this help screen'
-      say '[n]: next day'
-      say '[p]: previous day'
-      say '[t]: today'
+      say '[t]: next day'
+      say '[y]: previous day'
+      say '[n]: today'
       say '[x|q|exit|quit]: Exit interactive mode'
     end
 
