28a4df6b785cd66c575c30aa31f2995eb6f1c76f
gangelo <web.gma@gmail.com>
2023-06-17 22:36:11 -0400

    WIP migrations

--- lib/migrate/20230613121411_remove_and_add_configuration_attrs.rb
+++ lib/migrate/20230613121411_remove_and_add_configuration_attrs.rb
@@ -19,14 +19,7 @@ module Dsu
 
         # No sense in updating the configuration if it's not saved to disk.
         if File.exist?(config_path)
-          config_path = Support::Fileable.config_path
-          old_config_hash = Psych.safe_load(File.read(config_path), [Symbol]).transform_keys(&:to_sym)
-          config_hash = Dsu::Models::Configuration::DEFAULT_CONFIGURATION.merge(old_config_hash)
-          config_hash[:entries_display_order] = config_hash[:entries_display_order].to_sym
-          config_hash.delete('entries_file_name')
-          config_hash.delete('entries_folder')
-          config_hash[:version] = migration_version
-          Models::Configuration.instance.load(config_hash: config_hash).save!
+          update_configuration!
         else
           Models::Configuration.instance
         end
@@ -44,6 +37,17 @@ module Dsu
 
       def migration_version
         File.basename(__FILE__).match(Migration::MIGRATION_VERSION_REGEX).try(:[], 0)&.to_i
+      end
+
+      def update_configuration!
+        config_path = Support::Fileable.config_path
+        old_config_hash = Psych.safe_load(File.read(config_path), [Symbol]).transform_keys(&:to_sym)
+        config_hash = Dsu::Models::Configuration::DEFAULT_CONFIGURATION.merge(old_config_hash)
+        config_hash[:entries_display_order] = config_hash[:entries_display_order].to_sym
+        config_hash.delete('entries_file_name')
+        config_hash.delete('entries_folder')
+        config_hash[:version] = migration_version
+        Models::Configuration.instance.load(config_hash: config_hash).save!
       end
     end
   end
