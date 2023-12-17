3a94b55333a5218931e0f32081c673530e93e75f
gangelo <web.gma@gmail.com>
2023-12-17 14:57:00 -0500

    Refactor help colorizeable modules into 1 module

--- lib/dsu/support/command_help_colorizeable.rb
+++ lib/dsu/support/command_help_colorizeable.rb
@@ -12,7 +12,14 @@ module Dsu
         end
 
         module ClassMethods
+          # Handles general help colorization.
           def help(shell, subcommand = false) # rubocop:disable Style/OptionalBooleanParameter
+            help_text = Services::StdoutRedirectorService.call { super }
+            puts apply_theme(help_text, theme_color: color_theme.help)
+          end
+
+          # Handles sub-command help colorization.
+          def command_help(shell, subcommand = false) # rubocop:disable Style/OptionalBooleanParameter
             help_text = Services::StdoutRedirectorService.call { super }
             puts apply_theme(help_text, theme_color: color_theme.help)
           end
