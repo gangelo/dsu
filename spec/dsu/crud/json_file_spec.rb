7047370b7cb0141ddb1df407701cc5e3ca55e019
gangelo <web.gma@gmail.com>
2023-12-22 16:50:41 -0500

    dsu browse command round 1

--- spec/dsu/crud/json_file_spec.rb
+++ spec/dsu/crud/json_file_spec.rb
@@ -140,7 +140,7 @@ RSpec.describe Dsu::Crud::JsonFile do
       end
     end
 
-    context 'when the validation fails' do
+    context 'when the validation fails' do # rubocop:disable RSpec/MultipleMemoizedHelpers
       subject(:json_file_write) { json_file.write! }
 
       let(:file_data) { nil }
