844c173932910e9008b06014e1a7b69b1d579355
gangelo <web.gma@gmail.com>
2024-01-07 14:37:26 -0500

    Add ability to make screenshots for the dsu wiki easier

--- lib/dsu/env.rb
+++ lib/dsu/env.rb
@@ -2,7 +2,7 @@
 
 module Dsu
   class << self
-    def env
+    def env # rubocop:disable Metrics/MethodLength
       @env ||= Struct.new(:env) do
         def test?
           env.fetch('DSU_ENV', nil) == 'test'
@@ -18,6 +18,25 @@ module Dsu
 
         def production?
           env.fetch('DSU_ENV', 'production') == 'production'
+        end
+
+        def screen_shot_mode?
+          development? && (env.fetch('SCREEN_SHOT_USERNAME', '').present? ||
+            env.fetch('SCREEN_SHOT_HOSTNAME', '').present?)
+        end
+
+        def screen_shot_prompt
+          username = screen_shot_username
+          hostname = screen_shot_hostname
+          "#{username}@#{hostname}:~ $"
+        end
+
+        def screen_shot_username
+          env.fetch('SCREEN_SHOT_USERNAME', 'username')
+        end
+
+        def screen_shot_hostname
+          env.fetch('SCREEN_SHOT_HOSTNAME', 'hostname')
         end
       end.new(ENV)
     end
