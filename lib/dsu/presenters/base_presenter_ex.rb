0294888325b2b658c872bd490ae97595e36546ca
gangelo <web.gma@gmail.com>
2024-01-29 08:04:26 -0500

    Remove reference to color themes in base presenter

--- lib/dsu/presenters/base_presenter_ex.rb
+++ lib/dsu/presenters/base_presenter_ex.rb
@@ -1,26 +1,15 @@
 # frozen_string_literal: true
 
-require 'delegate'
-require_relative '../models/color_theme'
-require_relative '../support/color_themable'
-
 module Dsu
   module Presenters
     class BasePresenterEx
-      include Support::ColorThemable
-
       def initialize(options: {})
         @options = options&.dup || {}
-        @color_theme = Models::ColorTheme.find(theme_name: theme_name)
       end
 
       private
 
-      attr_reader :color_theme, :options
-
-      def theme_name
-        @theme_name ||= options.fetch(:theme_name, Models::Configuration.new.theme_name)
-      end
+      attr_reader :options
     end
   end
 end
